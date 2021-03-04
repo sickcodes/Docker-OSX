e03f0737c3ce4428047808c367409396cfa9e7ff
sickcodes <info@sick.codes>
2021-03-04 11:59:40 +0000

    Fix `.//config-custom.plist`

--- custom/generate-specific-bootdisk.sh
+++ custom/generate-specific-bootdisk.sh
@@ -194,11 +194,11 @@ generate_bootdisk () {
     elif [[ "${MASTER_PLIST}" ]] && [[ "${MASTER_PLIST_URL}" ]]; then
         echo 'You specified both a custom plist file AND a custom plist url. Use one or the other.'
     elif [[ "${MASTER_PLIST_URL}" ]]; then
-        wget -O "./${MASTER_PLIST:=/config-custom.plist}" "${MASTER_PLIST_URL}" \
+        wget -O "${MASTER_PLIST:=./config-custom.plist}" "${MASTER_PLIST_URL}" \
             || echo "Could not download ${MASTER_PLIST_URL}" && exit 1
     else
         MASTER_PLIST_URL='https://raw.githubusercontent.com/sickcodes/Docker-OSX/master/custom/config-nopicker-custom.plist'
-        wget -O "./${MASTER_PLIST:=/config-nopicker-custom.plist}" "${MASTER_PLIST_URL}" \
+        wget -O "${MASTER_PLIST:=./config-nopicker-custom.plist}" "${MASTER_PLIST_URL}" \
             || echo "Could not download ${MASTER_PLIST_URL}" && exit 1
     fi
 
