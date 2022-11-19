fbe5dba6188b949e8787ec9fa0d2957b4d52cceb
sickcodes <info@sick.codes>
2022-11-19 03:53:32 +0000

    Fix busted docker.list

--- tests/test.sh
+++ tests/test.sh
@@ -159,6 +159,7 @@ install_docker () {
     ; apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y \
     && curl -fsSL https://download.docker.com/linux/ubuntu/gpg |  apt-key add - \
     && apt-key fingerprint 0EBFCD88 \
+    && > /etc/apt/sources.list.d/docker.list \
     && add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
     && apt update -y \
     && apt install docker-ce docker-ce-cli containerd.io -y \
